72cab0c4347bcd2d44a0d9507fde017e0589dfb6
mairagalvao <mairagalvao01@gmail.com>
2023-11-19 17:22:23 -0300

    Feat: create graph grid Plateou with pointer

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,34 @@ import React, { useState } from 'react';
 import './App.css';
 import Rover from './Components/Rover';
 import UserData from './Components/UserData';
+import GridPlateou from './Components/GridPlateou';
 
 function App() {
-  const [roverData, setRoverData] = useState({ direction: 0, x: 0, y: 0, instructions: '' });
+
+  const [roverData, setRoverData] = useState({ direction: 0, x: 2, y: 4, instructions: '' });
 
   const handleSendData = (direction: number, x: number, y: number, instructions: string) => {
     setRoverData({ direction, x, y, instructions });
   };
 
+
+
+
   return (
     <>
-      <div className="App">
-        <div className='UserInfoRover'>
-          <UserData onSendData={handleSendData} />
-        </div>
-        <Rover
+   {/* <Rover
           direction={roverData.direction}
           x={roverData.x}
           y={roverData.y}
           instructions={roverData.instructions}
         />
-      </div>
+
+          <UserData onSendData={handleSendData} />
+       */}
+    <GridPlateou
+
+      />
+       
     </>
   );
 }
--- src/Components/GridPlateou.tsx
+++ src/Components/GridPlateou.tsx
@@ -1,13 +1,42 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Scatter } from 'react-chartjs-2';
+
+export function GridPlateou (){
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+
+ const options = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
 
 
-function GridPlateou() {
-  return (
-    <div className="GridPlateou">
-        <p>
-Grid Plateou        </p>
+const roversRange = []
+ const data = {
+  datasets: [
+    {
+      label: "Rover's Plateou",
+      data: [{x:10, y:6}, {x:2, y:3}, {x:12, y:11}],
+      backgroundColor: 'rgba(255, 99, 132, 1)',
+      pointStyle: "triangle",
+      radius:20
+      
 
-    </div>
-  );
+    },
+  ],
+};
+
+return <Scatter options={options} data={data} />
+
 }
-
-export default GridPlateou;
+export default GridPlateou
