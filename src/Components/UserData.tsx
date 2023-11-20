803557de96f299771be9666bbf893fad2c320890
mairagalvao <mairagalvao01@gmail.com>
2023-11-20 09:54:27 -0300

    feat: Enhance user interface to display final rover position after the instructions being followed

--- src/Components/UserData.tsx
+++ src/Components/UserData.tsx
@@ -13,6 +13,9 @@ const UserData = ({ onSendData }: UserDataProps) => {
   const [sizeYInput, setSizeYInput] = useState('');
   const [instructionsInput, setInstructionsInput] = useState('');
   const [userDataArray, setUserDataArray] = useState<any[]>([]);
+  const [finalDirection, setFinalDirection] = useState<number | null>(null);
+  const [finalX, setFinalX] = useState<number | null>(null);
+  const [finalY, setFinalY] = useState<number | null>(null);
   const [step, setStep] = useState<number>(0);
 
   const mapDirectionToNumber = (direction: string): number => {
@@ -27,6 +30,21 @@ const UserData = ({ onSendData }: UserDataProps) => {
         return 270;
       default:
         return parseFloat(direction) || 0;
+    }
+  };
+
+  const mapNumberToDirection = (numericDirection: number): string => {
+    switch (numericDirection) {
+      case 0:
+        return 'N';
+      case 90:
+        return 'E';
+      case 180:
+        return 'S';
+      case 270:
+        return 'W';
+      default:
+        return numericDirection.toString();
     }
   };
 
@@ -61,7 +79,7 @@ const UserData = ({ onSendData }: UserDataProps) => {
       setStep(1);
     } else if (step === 1) {
       const numericDirection = mapDirectionToNumber(directionInput);
-      console.log(`Sending Data to Rover: Direction: ${numericDirection}, X: ${xInput}, Y: ${yInput}, SizeX: ${sizeXInput}, SizeY: ${sizeYInput}, Instructions: ${instructionsInput}`);
+      console.log(`Sending Data to Rover: Direction: ${mapNumberToDirection(numericDirection)}, X: ${xInput}, Y: ${yInput}, SizeX: ${sizeXInput}, SizeY: ${sizeYInput}, Instructions: ${instructionsInput}`);
       onSendData(
         numericDirection,
         parseFloat(xInput),
@@ -74,7 +92,7 @@ const UserData = ({ onSendData }: UserDataProps) => {
       setUserDataArray((prevArray) => [
         ...prevArray,
         {
-          direction: numericDirection,
+          direction: mapNumberToDirection(numericDirection),
           x: parseFloat(xInput),
           y: parseFloat(yInput),
           sizeX: parseFloat(sizeXInput),
@@ -129,13 +147,13 @@ const UserData = ({ onSendData }: UserDataProps) => {
       }
     }
 
-    console.log(`Sending Data to Rover: Direction: ${newDirection}, X: ${newX}, Y: ${newY}, Instructions: ${instructionsInput}`);
+    console.log(`Sending Data to Rover: Direction: ${mapNumberToDirection(newDirection)}, X: ${newX}, Y: ${newY}, Instructions: ${instructionsInput}`);
     onSendData(newDirection, newX, newY, parseFloat(sizeXInput), parseFloat(sizeYInput), instructionsInput);
 
     setUserDataArray((prevArray) => [
       ...prevArray,
       {
-        direction: newDirection,
+        direction: mapNumberToDirection(newDirection),
         x: newX,
         y: newY,
         sizeX: parseFloat(sizeXInput),
@@ -144,6 +162,9 @@ const UserData = ({ onSendData }: UserDataProps) => {
       },
     ]);
 
+    setFinalDirection(newDirection);
+    setFinalX(newX);
+    setFinalY(newY);
     setStep(0);
   };
 
@@ -152,30 +173,25 @@ const UserData = ({ onSendData }: UserDataProps) => {
       {step === 0 && (
         <form id='x-y-inputs' onSubmit={handleButtonClick}>
           <label>
-            Enter size X of plateau:
-            <input type="text" value={sizeXInput} onChange={handleSizeXInput} required />
+            Enter X Dimension of Plateau: <input type="text" value={sizeXInput} onChange={handleSizeXInput} required />
           </label>
           <label>
-            Enter size Y of plateau:
-            <input type="text" value={sizeYInput} onChange={handleSizeYInput} required />
+            Enter Y Dimension of Plateau: <input type="text" value={sizeYInput} onChange={handleSizeYInput} required />
           </label>
-          <button type="submit">Next</button>
+          <button type="submit">Choose</button>
         </form>
       )}
 
       {step === 1 && (
         <form id='rover-position' onSubmit={handleButtonClick}>
           <label>
-            Enter landing direction:
-            <input type="text" value={directionInput} onChange={handleDirectionInput} required />
+            Enter X Coordinate for Rover: <input type="text" value={xInput} onChange={handleXInput} required />
           </label>
           <label>
-            Enter X coordinate:
-            <input type="text" value={xInput} onChange={handleXInput} required />
+            Enter Y Coordinate for Rover: <input type="text" value={yInput} onChange={handleYInput} required />
           </label>
           <label>
-            Enter Y coordinate:
-            <input type="text" value={yInput} onChange={handleYInput} required />
+            Enter Rover Direction on Landing: <input type="text" value={directionInput} onChange={handleDirectionInput} required />
           </label>
           <button type="submit">Next</button>
         </form>
@@ -184,24 +200,20 @@ const UserData = ({ onSendData }: UserDataProps) => {
       {step === 2 && (
         <form id='instructions-input' onSubmit={handleInstructionsSubmit}>
           <label>
-            Enter instructions (LRM string):
-            <input type="text" value={instructionsInput} onChange={handleInstructionsInput} required />
+            Enter Movement Instructions (LRM string): <input type="text" value={instructionsInput} onChange={handleInstructionsInput} required />
           </label>
-          <button type="submit">Start Instructions</button>
+          <button type="submit">Start</button>
         </form>
       )}
 
-      {/* Display User Data Array */}
-      <div className="user-data-array">
-        <h2>User Data Array</h2>
-        <ul>
-          {userDataArray.map((userData, index) => (
-            <li key={index}>
-              Direction: {userData.direction}, X: {userData.x}, Y: {userData.y}, SizeX: {userData.sizeX}, SizeY: {userData.sizeY}, Instructions: {userData.instructions}
-            </li>
-          ))}
-        </ul>
-      </div>
+      {finalDirection !== null && (
+        <div className="final-direction">
+          <h2>Final Rover's Position</h2>
+          <p>Final Position: {finalX} {finalY} {mapNumberToDirection(finalDirection)}</p>
+        </div>
+      )}
+
+      
     </div>
   );
 };
